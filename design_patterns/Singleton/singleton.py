b6458a0a46ad5a3914d554945107639adcc9b177
Pavan-Aditya-Kumar-Gorrela <kumarpavanaditya@gmail.com>
2026-08-19 17:58:22 +0530

    19 aug 2026 SingleTon Design Pattern POC

---
+++ design_patterns/Singleton/singleton.py
@@ -0,0 +1,26 @@
+from typing import Self
+
+from ultralytics import YOLO
+
+class YOLOModel:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            print("Calling YOLO...")
+            cls._instance = super().__new__(cls)
+            cls._instance.model = YOLO("yolo11n.pt")
+        return cls._instance
+
+class ImageService:
+    def __init__(self):
+        self.yolo = YOLOModel()
+
+class VideoService:
+    def __init__(self):
+        self.yolo = YOLOModel()
+
+img = ImageService()
+vid = VideoService()
+
+print(img.yolo.model is vid.yolo.model)
